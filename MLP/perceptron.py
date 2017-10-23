40c7d2560f1ddb31465a6b20d737aecaa7a8af81
mordowiciel <szymar95@gmail.com>
2017-10-23 23:21:26 +0200

    implemented signal propagation algorithm

--- MLP/perceptron.py
+++ MLP/perceptron.py
@@ -2,21 +2,29 @@ import numpy as np
 
 
 class Perceptron:
-
     def __init__(self):
-        self.weights = np.random.rand(1, 2)
+        self.weights = []
         self.output = 0
 
     def calculate_output_with_linear(self, input):
+
+        if len(self.weights) == 0:
+            print 'CALCULATING RANDOM WEIGHTS'
+            self.weights = np.random.rand(len(input))
+
         return np.sum(self.weights * input)
 
     def calculate_output_with_sigmoidal(self, input):
+
+        if len(self.weights) == 0:
+            print 'CALCULATING RANDOM WEIGHTS'
+            self.weights = np.random.rand(len(input))
+
         neuron_sum = np.sum(self.weights * input)
         return neuron_sum * (1 - neuron_sum)
 
 
 class Layer:
-
     def __init__(self, number_of_perceptrons=0):
         self.perceptrons = []
         for x in range(0, number_of_perceptrons):
@@ -24,7 +32,6 @@ class Layer:
 
 
 class Network:
-
     def __init__(self, layers=[]):
         self.layers = layers
 
@@ -35,37 +42,36 @@ class Network:
         return 0
 
 
-
-# 1) oblicz output dla kazdego neuronu w warstwie
-# 2) wez kazdy output i stworz z niego wektor wejsc
-# 3) dla kazdego neuronu w kolejnej warstwie przyporzadkuj powyzsza tablice na wejscie
-
+# Network setup
 introLayer = Layer(5)
 midLayer = Layer(1)
+endLayer = Layer(3)
+network = Network([introLayer, midLayer, endLayer])
 
-dataInput = np.array([[1.0, 1.0]])
+# Input setup
+# TODO : Implement the correct way to get the length of input vector in order to calculate product of weights and input.
+dataInput = np.array([[1, 1]])
 
+for layer_index, current_layer in enumerate(network.layers):
 
-# 1) Calculate output for every neuron in the layer.
-layer_outputs = []
-for index, neuron in enumerate(introLayer.perceptrons):
+    print 'Calculating output for ' + str(layer_index) + ' layer'
 
-    print 'Calculating output for ' + str(index) + ' neuron'
-    print 'Neuron weights are: '
-    print neuron.weights
+    # Calculate output of current layer
+    layer_outputs = []
+    for index, neuron in enumerate(current_layer.perceptrons):
 
-    neuron_output = neuron.calculate_output_with_linear(dataInput)
-    print 'Neuron output is '
-    print neuron_output
-    neuron.output = neuron_output
+        print 'Calculating output for ' + str(index) + ' neuron'
+        neuron_output = neuron.calculate_output_with_linear(dataInput)
 
+        print 'Neuron weights are: '
+        print neuron.weights
 
-    layer_outputs.append(neuron_output)
-    print ''
+        print 'Neuron output is '
+        print neuron_output
+        neuron.output = neuron_output
 
+        layer_outputs.append(neuron_output)
+        print ''
 
-print 'Array of outputs of layer is: '
-print layer_outputs
-
-
-
+    # Set the output of current layer to the next layer
+    dataInput = layer_outputs
