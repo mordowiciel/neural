7dd3dc33e1e7bf8f4267fb9b30cb189b2e40a9cd
mordowiciel <szymar95@gmail.com>
2018-01-02 21:30:16 +0100

    added distance functions

--- knn/distance_utils.py
+++ knn/distance_utils.py
@@ -1,7 +1,7 @@
 import math
 
 
-def calculate_euclidean_distance(arr1, arr2):
+def euclidean_distance(arr1, arr2):
 
     if(arr1.size != arr2.size):
         raise ValueError("Arrays vary in size")
@@ -11,3 +11,27 @@ def calculate_euclidean_distance(arr1, arr2):
         distance_sum += pow((arr1[n] - arr2[n]), 2)
 
     return math.sqrt(distance_sum)
+
+
+def manhattan_distance(arr1, arr2):
+
+    if(arr1.size != arr2.size):
+        raise ValueError("Arrays vary in size")
+
+    distance_sum = 0
+    for n in range(0, arr1.size):
+        distance_sum += abs(arr1[n] - arr2[n])
+
+    return distance_sum
+
+
+def chebyshev_distance(arr1, arr2):
+
+    if(arr1.size != arr2.size):
+        raise ValueError("Arrays vary in size")
+
+    result_arr = []
+    for n in range(0, arr1.size):
+        result_arr.append(arr1[n] - arr2[n])
+
+    return max(result_arr)
--- knn/main.py
+++ knn/main.py
@@ -3,12 +3,21 @@
 # !/usr/bin/env python
 
 from sklearn.datasets import load_iris
-import distance_utils
+import distance_utils as du
 
 iris_dataset = load_iris()
 
 print iris_dataset.data[0]
 print iris_dataset.data[1]
 
-print distance_utils.calculate_euclidean_distance(iris_dataset.data[0],
-                                                  iris_dataset.data[1])
+print 'Euclidean distance: '
+print du.euclidean_distance(iris_dataset.data[0],
+                            iris_dataset.data[1])
+
+print 'Manhattan distance: '
+print du.manhattan_distance(iris_dataset.data[0],
+                            iris_dataset.data[1])
+
+print 'Chebyshev distance: '
+print du.chebyshev_distance(iris_dataset.data[0],
+                            iris_dataset.data[1])
