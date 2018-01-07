e2d33f7845a09097de56461e594c154a07850cb7
mordowiciel <szymar95@gmail.com>
2018-01-07 18:41:21 +0100

    added loading from CSV file

--- knn/knn.py
+++ knn/knn.py
@@ -1,5 +1,5 @@
-import distance_utils as du
 import heapq
+import distance_utils as du
 
 
 class KNN:
@@ -7,11 +7,13 @@ class KNN:
     knn_heap = []
     training_data = []
     test_data = []
+    classification_labels = set()
     k = 0
 
-    def __init__(self, training_data, test_data, k):
+    def __init__(self, training_data, test_data, classification_labels, k):
         self.training_data = training_data
         self.test_data = test_data
+        self.classification_labels = classification_labels
         self.k = k
 
     def init_point_heap(self, test_point):
@@ -26,16 +28,17 @@ class KNN:
         self.knn_heap = []
         self.init_point_heap(test_point)
 
-        class_count = [0, 0, 0]
+        # init dictionary
+        class_count = {}
+        for label in self.classification_labels:
+            class_count[label] = 0
+
+        # class_count = {'Iris-setosa': 0,
+        #                'Iris-versicolor': 0,
+        #                'Iris-virginica': 0}
+
         for i in range(0, self.k):
             neighbour = heapq.heappop(self.knn_heap)
+            class_count[neighbour[1]] += 1
 
-            if(neighbour[1] == 0):
-                class_count[0] += 1
-            if(neighbour[1] == 1):
-                class_count[1] += 1
-            if(neighbour[1] == 2):
-                class_count[2] += 1
-
-        point_class = class_count.index(max(class_count))
-        return point_class
+        return max(class_count, key=class_count.get)
