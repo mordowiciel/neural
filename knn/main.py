c11cead320ddbbe0971280b3776d975db76acee1
mordowiciel <szymar95@gmail.com>
2018-01-09 23:14:41 +0100

    main cleanup demo version of k cross valid

--- knn/main.py
+++ knn/main.py
@@ -4,46 +4,39 @@
 
 import numpy as np
 import pandas as pd
+import dataset_utils as du
 from sklearn import preprocessing
 from knn import KNN
 
 # Load dataset.
-dataset = pd.read_csv("wine.csv", header=None)
+dataset = pd.read_csv("iris.csv", header=None)
 data = dataset.as_matrix()
 np.random.shuffle(data)
 
 # Standarize the data.
-# data_points = np.delete(data, 4, axis=1)
-# preprocessing.scale(data_points)
-
-# Kocham Pythona za te one-linery <3 <3 <3
 data[:, :-1] = preprocessing.scale(data[:, :-1])
 
-# Create (point, label) tuple
-data_tuples = []
-class_labels = set([])
-for row in data:
+# Get classification labels from dataset,
+# convert matrix to (data, label) tuple.
+class_labels = du.get_classification_labels(data)
+data = du.convert_to_point_label_tuple(data)
 
-    # Extract labels from dataset
-    class_labels.add(row[-1])
+# Perform k-cross validation
+for i in range(0, 3):
 
-    row_tuple = (row[:-1], row[-1])
-    data_tuples.append(row_tuple)
+    print 10 * "*", "K = ", i, 10 * "*"
+    properly_classified = 0
 
-data = data_tuples
+    # Create training_data and test_data sets
+    training_data, test_data = du.divide_k_cross(data, i)
+    knn = KNN(training_data, test_data, class_labels, 3)
 
-training_data = data[:100]
-test_data = data[100:]
+    for point in test_data:
 
-properly_classified = 0
-knn = KNN(training_data, test_data, class_labels, 3)
+        classification_label = knn.classify_point(point[0])
+        print "Expected class:", point[1], "Result:", classification_label
 
-for point in test_data:
+        if(point[1] == classification_label):
+            properly_classified += 1
 
-    classification_label = knn.classify_point(point[0])
-    print "Expected class:", point[1], "Result:", classification_label
-
-    if(point[1] == classification_label):
-        properly_classified += 1
-
-print "Properly classifed : ", properly_classified, "/", len(test_data)
+    print "Properly classifed : ", properly_classified, "/", len(test_data)
